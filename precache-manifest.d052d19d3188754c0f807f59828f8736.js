d9237f1576cfe0a176b1ecbffd88b0b6c9f83ba0
deskoh <deskoh.sg@gmail.com>
2019-09-20 19:37:00 +0800

    Updates

---
+++ precache-manifest.d052d19d3188754c0f807f59828f8736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a433c341b71e9cf6096b192b6a650c7",
+    "url": "/sgpsi/index.html"
+  },
+  {
+    "revision": "60044dc0302899ff46a5",
+    "url": "/sgpsi/static/css/main.99944224.chunk.css"
+  },
+  {
+    "revision": "77094cf740d9e66fab00",
+    "url": "/sgpsi/static/js/2.480b9093.chunk.js"
+  },
+  {
+    "revision": "60044dc0302899ff46a5",
+    "url": "/sgpsi/static/js/main.a026ed94.chunk.js"
+  },
+  {
+    "revision": "a19bc1a82e6feaca1abd",
+    "url": "/sgpsi/static/js/runtime-main.4d326e79.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/sgpsi/static/media/github.617870e4.svg"
+  }
+]);
